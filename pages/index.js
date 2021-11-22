423fa29075d5d7bb75286ca9ba016f99be05d5e4
Kai Mavyn <94161518+saltspell@users.noreply.github.com>
2021-11-22 13:08:50 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kai Mavyn & Next.js!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
