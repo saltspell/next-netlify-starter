40a584ca05467653fdad9f4bddb810d678045c96
Kai Mavyn <kai@saltspell.io>
2021-11-22 14:07:46 -0800

    Adding Stuff

---
+++ pages/posts/technicalexercise.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function technicalexercise() {
+  return (
+    <>
+      <h1>Technical Exercise!</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
